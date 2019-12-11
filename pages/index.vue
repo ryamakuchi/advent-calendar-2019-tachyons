5824b21b6b0a8d2f81c80500b265de51e8156057
rry <r.yamakuchi@gmail.com>
2019-12-12 01:26:58 +0900

    add: calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,106 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        advent-calendar-2019-tachyons
-      </h1>
-      <h2 class="subtitle">
-        My kickass Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="pa3 ph5-l">
+    <h1>
+      Nuxt.js Advent Calendar 2019
+    </h1>
+
+    <p class="mb4 lh-copy">{{ comment }}</p>
+
+    <div class="flex flex-wrap ba b--black-10">
+      <div
+        v-for="dayName in dayNames"
+        :key="dayName"
+        class="calendar-item f4 bg-near-black white mv0 pv2 ph3"
+      >
+        {{ dayName }}
+      </div>
+
+      <article
+        v-for="day in days"
+        :key="day.date"
+        class="calendar-item ba b--black-10"
+      >
+        <p class="f4 bg-near-white black-60 mv0 pv2 ph3">
+          {{ day.date }}
+        </p>
+
+        <div class="pa3 tc">
+          <img
+            :src="day.authorImageUrl"
+            :alt="day.authorName"
+            class="br-100 h3 w3 dib"
+          >
+
+          <a
+            :href="`https://qiita.com/${day.authorName}`"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="db link hover-blue lh-copy measure center f6 black-70"
+          >
+            {{ day.authorName }}
+          </a>
+
+          <hr class="mw3 mv3 bb bw1 b--black-10 center">
+
+          <a v-if="day.articleUrl !== null"
+            :href="day.articleUrl"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="db blue link f6 f5-ns lh-copy measure tl dim"
+          >
+            {{ day.comment }}
+          </a>
+
+          <p v-else
+            class="f6 f5-ns lh-copy measure tl"
+          >
+            {{ day.comment }}
+          </p>
+        </div>
+      </article>
+
+      <div
+        v-for="dayName in [26, 27, 28]"
+        :key="dayName"
+        class="calendar-item ba b--black-10"
+      >
+        <p class="f4 bg-near-white black-60 mv0 pv2 ph3">
+          {{ dayName }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+import { parse } from 'node-html-parser'
+
+export default {
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get('https://qiita.com/advent-calendar/2019/nuxt-js')
+    const root = parse(data)
+    return {
+      comment: root.querySelector('.markdownContent').text,
+      dayNames: root.querySelectorAll('.adventCalendarCalendar_dayName').map(dayName => dayName.text),
+      days: root.querySelectorAll('.adventCalendarCalendar_day').map(day => {
+        const articleUrl = day.querySelector('.adventCalendarCalendar_comment').querySelector('a') ?
+          day.querySelector('.adventCalendarCalendar_comment').querySelector('a').attributes['href'] :
+          null
+        return {
+          date: day.querySelector('.adventCalendarCalendar_date').text,
+          authorName: day.querySelector('.adventCalendarCalendar_author').text.replace(/\s|&nbsp;/g, ''),
+          authorImageUrl: day.querySelector('.adventCalendarCalendar_authorIcon').attributes['src'],
+          comment: day.querySelector('.adventCalendarCalendar_comment').text,
+          articleUrl: articleUrl
+        }
+      })
+    }
+  }
+}
+</script>
+
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.calendar-item {
+  width: calc(100% / 7);
 }
 </style>
